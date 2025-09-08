a4f568a15270d4c85669f7ec6c39f8fad613ddb2
J2ANS0l0 <j2an.s0l0@gmail.com>
2025-09-07 19:32:00 -0600

    Diagonal Test ALL MATRIX 00-03

--- Diagonal.ino
+++ Diagonal.ino
@@ -9,7 +9,7 @@ const int R[] = {7,6,59,58,13,12,65,64,19,18,33,32,25,24,39,38};
 const int G[] = {5,4,57,56,11,10,63,62,17,16,69,68,23,22,37,36};
 const int B[] = {3,2,55,54,9,8,61,60,15,14,67,66,21,20,35,34};
 
-int frequency = 500;
+int frequency = 5;
 
 void setup() {
   Serial.begin(9600);
@@ -27,36 +27,87 @@ void setup() {
 
 void loop() {
 
-    digitalWrite(R[0], 1);
-    digitalWrite(B[0], 1);
+    digitalWrite(R[4*0+0], 1);
+    digitalWrite(B[4*0+0], 1);
+    digitalWrite(R[4*1+0], 1);
+    digitalWrite(B[4*1+0], 1);
+    digitalWrite(R[4*2+0], 1);
+    digitalWrite(B[4*2+0], 1);
+    digitalWrite(R[4*3+0], 1);
+    digitalWrite(B[4*3+0], 1);
     digitalWrite(GND[0], 1);
     delay(frequency);
     digitalWrite(GND[0], 0);
-    digitalWrite(R[0], 0);
-    digitalWrite(B[0], 0);
+    digitalWrite(R[4*0+0], 0);
+    digitalWrite(B[4*0+0], 0);
+    digitalWrite(R[4*1+0], 0);
+    digitalWrite(B[4*1+0], 0);
+    digitalWrite(R[4*2+0], 0);
+    digitalWrite(B[4*2+0], 0);
+    digitalWrite(R[4*3+0], 0);
+    digitalWrite(B[4*3+0], 0);
 
-    digitalWrite(R[1], 1);
-    digitalWrite(B[1], 1);
+
+    digitalWrite(R[4*0+1], 1);
+    digitalWrite(B[4*0+1], 1);
+    digitalWrite(R[4*1+1], 1);
+    digitalWrite(B[4*1+1], 1);
+    digitalWrite(R[4*2+1], 1);
+    digitalWrite(B[4*2+1], 1);
+    digitalWrite(R[4*3+1], 1);
+    digitalWrite(B[4*3+1], 1);
     digitalWrite(GND[1], 1);
     delay(frequency);
     digitalWrite(GND[1], 0);
-    digitalWrite(R[1], 0);
-    digitalWrite(B[1], 0);
+    digitalWrite(R[4*0+1], 0);
+    digitalWrite(B[4*0+1], 0);
+    digitalWrite(R[4*1+1], 0);
+    digitalWrite(B[4*1+1], 0);
+    digitalWrite(R[4*2+1], 0);
+    digitalWrite(B[4*2+1], 0);
+    digitalWrite(R[4*3+1], 0);
+    digitalWrite(B[4*3+1], 0);
 
 
-    digitalWrite(R[2], 1);
-    digitalWrite(B[2], 1);
+    digitalWrite(R[4*0+2], 1);
+    digitalWrite(B[4*0+2], 1);
+    digitalWrite(R[4*1+2], 1);
+    digitalWrite(B[4*1+2], 1);
+    digitalWrite(R[4*2+2], 1);
+    digitalWrite(B[4*2+2], 1);
+    digitalWrite(R[4*3+2], 1);
+    digitalWrite(B[4*3+2], 1);
     digitalWrite(GND[2], 1);
     delay(frequency);
     digitalWrite(GND[2], 0);
-    digitalWrite(R[2], 0);
-    digitalWrite(B[2], 0);
+    digitalWrite(R[4*0+2], 0);
+    digitalWrite(B[4*0+2], 0);
+    digitalWrite(R[4*1+2], 0);
+    digitalWrite(B[4*1+2], 0);
+    digitalWrite(R[4*2+2], 0);
+    digitalWrite(B[4*2+2], 0);
+    digitalWrite(R[4*3+2], 0);
+    digitalWrite(B[4*3+2], 0);
 
-    digitalWrite(R[3], 1);
-    digitalWrite(B[3], 1);
+
+    digitalWrite(R[4*0+3], 1);
+    digitalWrite(B[4*0+3], 1);
+    digitalWrite(R[4*1+3], 1);
+    digitalWrite(B[4*1+3], 1);
+    digitalWrite(R[4*2+3], 1);
+    digitalWrite(B[4*2+3], 1);
+    digitalWrite(R[4*3+3], 1);
+    digitalWrite(B[4*3+3], 1);
     digitalWrite(GND[3], 1);
     delay(frequency);
     digitalWrite(GND[3], 0);
-    digitalWrite(R[3], 0);
-    digitalWrite(B[3], 0);
+    digitalWrite(R[4*0+3], 0);
+    digitalWrite(B[4*0+3], 0);
+    digitalWrite(R[4*1+3], 0);
+    digitalWrite(B[4*1+3], 0);
+    digitalWrite(R[4*2+3], 0);
+    digitalWrite(B[4*2+3], 0);
+    digitalWrite(R[4*3+3], 0);
+    digitalWrite(B[4*3+3], 0);
+
 }
